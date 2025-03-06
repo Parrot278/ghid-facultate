83c2f626c7f802d36cfe0966fdbb4263b28328d0
Parrot278 <100129897+Parrot278@users.noreply.github.com>
2025-03-06 06:47:41 +0000

    Edited urls.py

--- universities/urls.py
+++ universities/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("chestionar/", views.chestionar, name = "chestionar"),
     path("bibliografie/", views.bibliografie, name = "bibliografie"),
     path("cvmeniu/", views.cv_meniu, name = "cvmeniu"),
+    path("<int:facultate_id>/detaliifacultate/", views.detalii_facultate, name="detaliifacultate")
 ]
