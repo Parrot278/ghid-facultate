55c9327d796ec0a6562b1300638f76aca8de87c7
Parrot278 <sofi.tudose@gmail.com>
2025-04-06 16:48:28 +0000

    Test debug cand e false

--- university_guide/settings.py
+++ university_guide/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-**(arts()x9gp+kd5w%y75kxy^(bt5$l!r*64$b52_1eld=o3j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
